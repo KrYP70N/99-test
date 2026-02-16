61d8b335045b1db15eb91b1f09d83c4d74809753
aiden <kyawmyo.h@7peakssoftware.com>
2026-02-16 18:34:40 +0700

    fix path

--- solution/vite.config.ts
+++ solution/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for better portability on GH Pages
+  base: '/99-test/', // Explicitly set the repo name for GitHub Pages
 })
